6d0a3075bec71a0377e4f293d40df58dac3e98af
oksa satya <oksasatyaa@gmail.com>
2022-01-03 12:55:08 +0700

    add book table

---
+++ database/migrations/2022_01_03_051329_create_book_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('description');
+            $table->string('author');
+            $table->string('publisher');
+            $table->string('cover');
+            $table->float('price');
+            $table->integer('weight');
+            $table->integer('views');
+            $table->integer('stock')->unsigned()->default(0);
+            $table->enum('status', ['PUBLISH', 'DRAFT'])->default('PUBLISH');
+            $table->timestamps(); // created_at, updated_at
+            $table->softDeletes(); // deleted_at
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->integer('deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book');
+    }
+}
